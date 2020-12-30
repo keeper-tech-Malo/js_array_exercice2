61790acfa516ee48400c99f55137d30f1333e85e
Malo <ndekezi37@gmail.com>
2020-12-30 14:59:24 +0100

    fin du travail

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+let tableaux = ["apple", "pomme", "banane"];
+console.log(tableaux);
+
+var newlength = tableaux.push('Malo')
+console.log(tableaux)
+
+
+
+// tableaux.forEach(function(item, index, array) {
+//     console.log(item,index)
+// })
+
+
+
